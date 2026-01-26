658aa569294875167b230b220f433de325b87e2a
macsavasi <lahmacundunyasi13@gmail.com>
2026-01-27 00:23:37 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 
 export DISPLAY=:99
 
-# Virtual ekran
-Xvfb :99 -screen 0 1280x720x24 &
+# DİKEY sanal ekran (9:16)
+Xvfb :99 -screen 0 1080x1920x24 &
 sleep 4
 
-# Cursor gizle (siyah ekrandaki işaret gider)
+# Cursor gizle
 xsetroot -cursor_name left_ptr || true
 
-# Chromium’u render zorlayarak başlat
+# Chromium DİKEY açılıyor
 chromium-browser \
   --no-sandbox \
   --disable-gpu \
@@ -19,27 +19,26 @@ chromium-browser \
   --disable-default-apps \
   --disable-extensions \
   --disable-sync \
-  --metrics-recording-only \
   --no-first-run \
   --autoplay-policy=no-user-gesture-required \
-  --window-size=1280,720 \
+  --window-size=1080,1920 \
   --force-device-scale-factor=1 \
   --kiosk \
   --hide-scrollbars \
   --mute-audio \
   "https://macsavasi.github.io/macsavasi/" &
 
-# Chromium’un gerçekten çizmesi için bekle
 sleep 10
 
-# YouTube yayını
+# YouTube Shorts akışı
 ffmpeg -re \
-  -f x11grab -video_size 1280x720 -framerate 30 -i :99 \
+  -f x11grab -video_size 1080x1920 -framerate 30 -i :99 \
   -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 \
   -c:v libx264 -preset veryfast -tune zerolatency \
   -pix_fmt yuv420p \
-  -b:v 3000k -maxrate 3000k -bufsize 6000k \
+  -profile:v high -level 4.2 \
+  -b:v 4500k -maxrate 4500k -bufsize 9000k \
   -g 60 \
   -c:a aac -b:a 128k \
   -f flv \
-  rtmp://a.rtmp.youtube.com/live2/rwbf-by2t-h4xe-39rg-ahz7
+  rtmp://a.rtmp.youtube.com/live2/YOUTUBE_STREAM_KEY
