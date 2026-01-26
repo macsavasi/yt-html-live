1d3875c1a8e37086c49deb4a4b23f7a6c91a3571
macsavasi <lahmacundunyasi13@gmail.com>
2026-01-27 00:18:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,27 +2,43 @@
 
 export DISPLAY=:99
 
+# Virtual ekran
 Xvfb :99 -screen 0 1280x720x24 &
-sleep 3
+sleep 4
 
+# Cursor gizle (siyah ekrandaki işaret gider)
+xsetroot -cursor_name left_ptr || true
+
+# Chromium’u render zorlayarak başlat
 chromium-browser \
   --no-sandbox \
   --disable-gpu \
+  --disable-software-rasterizer \
   --disable-dev-shm-usage \
+  --disable-background-networking \
+  --disable-default-apps \
+  --disable-extensions \
+  --disable-sync \
+  --metrics-recording-only \
+  --no-first-run \
   --autoplay-policy=no-user-gesture-required \
   --window-size=1280,720 \
+  --force-device-scale-factor=1 \
   --kiosk \
   --hide-scrollbars \
   --mute-audio \
   "https://macsavasi.github.io/macsavasi/" &
 
-sleep 6
+# Chromium’un gerçekten çizmesi için bekle
+sleep 10
 
+# YouTube yayını
 ffmpeg -re \
   -f x11grab -video_size 1280x720 -framerate 30 -i :99 \
   -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 \
-  -c:v libx264 -preset veryfast -tune zerolatency -pix_fmt yuv420p \
-  -b:v 2500k -maxrate 2500k -bufsize 5000k \
+  -c:v libx264 -preset veryfast -tune zerolatency \
+  -pix_fmt yuv420p \
+  -b:v 3000k -maxrate 3000k -bufsize 6000k \
   -g 60 \
   -c:a aac -b:a 128k \
   -f flv \
